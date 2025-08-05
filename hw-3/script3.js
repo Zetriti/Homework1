a488b88519d2efe61002ae9d969823a7b98d6b74
Zetrit <dima.228_1@mail.ru>
2025-08-05 23:37:00 +0300

    Домашка 4

---
+++ hw-3/script3.js
@@ -0,0 +1,8 @@
+console.log('Задание 3');
+let d = 101; console.log('Первое число = '+ d);
+let e = 91; console.log('Второе число = '+ e);
+if (d > 100 || e > 100) {
+    console.log("Верно");
+} else {
+    console.log("Неверно"); // Выведет "Неверно"
+}
